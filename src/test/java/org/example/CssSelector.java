51b17eec02f9ab99417f251be640966f7770bbd6
suhailkt <suhailktmit@gmail.com>
2023-04-17 12:38:21 +0530

    css done

--- src/test/java/org/example/CssSelector.java
+++ src/test/java/org/example/CssSelector.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,13 +9,33 @@ public class CssSelector {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.http.factory", "jdk-http-client");
         WebDriverManager.chromedriver().setup();
-        WebDriver driver= new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
 
         //open app
-        driver.get("https://admin-demo.nopcommerce.com/");
+        driver.get("https://demo.nopcommerce.com/");
         Thread.sleep(5000);
         driver.manage().window().maximize();
 
+        //Title validation
+        String act_title = driver.getTitle();
+        String exp_title = "nopCommerce demo store";
+        if (act_title.equals(exp_title)) {
+            System.out.println("Title is same");
+        } else {
+            System.out.println("Title not same");
 
+        }
+
+        //tag, id
+//        driver.findElement(By.cssSelector("input#small-searchterms")).sendKeys("Mac");
+
+        //tag,class
+//        driver.findElement(By.cssSelector("input.search-box-text")).sendKeys("Mac");
+
+        //tag, attribute
+//        driver.findElement(By.cssSelector("input[name='q']")).sendKeys("Mac");
+
+        //tag,class,attribute
+        driver.findElement(By.cssSelector("input.search-box-text[name='q']")).sendKeys("Mac");
     }
 }
